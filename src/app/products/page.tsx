a5f778a484e995f0a5537b163c2a337cef51ce0b
goncalo1212 <a26269@aemtg.pt>
2025-09-17 15:57:27 +0100

    update: algumas coisas

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,16 @@
 // src/app/products/page.tsx
 import { Suspense } from "react";
-import dynamic from "next/dynamic";
-
-const ProductsClient = dynamic(() => import("./ProductsClient"), {
-  ssr: false, // garante que qualquer hook de navegação só corre no cliente
-});
+import ProductsClient from "./ProductsClient";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div className="container-fw py-16">Loading…</div>}>
+    <Suspense
+      fallback={
+        <div className="container-fw py-16">
+          <p>Loading…</p>
+        </div>
+      }
+    >
       <ProductsClient />
     </Suspense>
   );
