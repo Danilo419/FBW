8abe4f12d567023edf31cae27cfe987db786d54b
goncalo1212 <a26269@aemtg.pt>
2025-10-20 20:46:54 +0100

    update: algumas coisas

--- src/app/products/[slug]/product-configurator.tsx
+++ src/app/products/[slug]/product-configurator.tsx
@@ -97,7 +97,7 @@ export default function ProductConfigurator({ product }: { product: ProductClien
       return;
     }
 
-    // Integra aqui com a tua store/API de carrinho
+    // Integra aqui com a tua store/API do carrinho
     console.log("ADD TO CART", {
       productId: product.id,
       slug: product.slug,
