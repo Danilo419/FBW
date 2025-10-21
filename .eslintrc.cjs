20bb33771f22e43a1c8e56eb16377285eaec0513
goncalo1212 <a26269@aemtg.pt>
2025-10-21 18:04:19 +0100

    update: algumas coisas

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,16 +2,29 @@
 module.exports = {
   root: true,
   extends: ["next/core-web-vitals"],
+  parser: require.resolve("@typescript-eslint/parser"),
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
+    project: undefined // evita erros de configuração em builds
   },
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
-    "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+    ],
     "react-hooks/exhaustive-deps": "warn",
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@next/next/no-html-link-for-pages": "off",
     "prefer-const": "off",
     "react/no-unescaped-entities": "off"
-  }
+  },
+  ignorePatterns: [
+    "node_modules/",
+    ".next/",
+    "out/",
+    "build/",
+    "next-env.d.ts"
+  ]
 };
